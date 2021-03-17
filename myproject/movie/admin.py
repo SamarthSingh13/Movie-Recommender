661a2fede09f2204c97a4bb4c07df8397ae988c8
Ayush Garg <ayushgarg20000@gmail.com>
2021-03-17 17:11:51 +0530

    added movie app files

---
+++ myproject/movie/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Show, Episode, Ott, Genre, Language, Country, Person, CastMember, CrewMember
+
+# Register your models here.
+admin.site.register(Show)
+admin.site.register(Episode)
+admin.site.register(Ott)
+admin.site.register(Language)
+admin.site.register(Genre)
+admin.site.register(Country)
