58db95343344f9b0e9d00abbd9ec15c7ccd3cb07
taufeeque9 <9taufeeque9@gmail.com>
2021-05-05 22:14:12 +0530

    Add template files

---
+++ myproject/recommender/views.py
@@ -0,0 +1,194 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import *
+from django.http import Http404
+from .models import Ott, Genre, Language, Country, Show, OriginCountry, OriginalLanguage, Genre_of
+from .models import Available, Rating, Person, User, Director, Actor, LangPreference, WatchList, Mru
+from django.db.models import Q
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.db.models import Case, When
+import pandas as pd
+
+# Create your views here.
+def index(request):
+    shows = Show.objects.all()
+    query = request.GET.get('q')
+
+    if query:
+        shows = Show.objects.filter(Q(title__icontains=query)).distinct()
+        return render(request, 'search.html', {'shows': shows})
+
+    return render(request, 'home.html', {'shows': shows})
+
+# Show the search result
+def search(request):
+    shows = Show.objects.all()
+    query = request.GET.get('q')
+
+    if query:
+        shows = Show.objects.filter(Q(title__icontains=query)).distinct()
+        return render(request, 'search.html', {'shows': shows})
+
+    return render(request, 'home.html', {'shows': shows})
+
+# Show details of the movie
+def detail(request, movie_id):
+    
+    if not request.user.is_active:
+        raise Http404
+    movies = get_object_or_404(Show, id=movie_id)
+    movie = Show.objects.get(id=movie_id)
+    
+    temp = list(MyList.objects.all().values().filter(movie_id=movie_id,user=request.user))
+    if temp:
+        update = temp[0]['watch']
+    else:
+        update = False
+    if request.method == "POST":
+
+        # For my list
+        if 'watch' in request.POST:
+            watch_flag = request.POST['watch']
+            if watch_flag == 'on':
+                update = True
+            else:
+                update = False
+            if MyList.objects.all().values().filter(movie_id=movie_id,user=request.user):
+                MyList.objects.all().values().filter(movie_id=movie_id,user=request.user).update(watch=update)
+            else:
+                q=MyList(user=request.user,movie=movie,watch=update)
+                q.save()
+            if update:
+                messages.success(request, "Show added to your list!")
+            else:
+                messages.success(request, "Show removed from your list!")
+
+            
+        # For rating
+        else:
+            rate = request.POST['rating']
+            if Myrating.objects.all().values().filter(movie_id=movie_id,user=request.user):
+                Myrating.objects.all().values().filter(movie_id=movie_id,user=request.user).update(rating=rate)
+            else:
+                q=Myrating(user=request.user,movie=movie,rating=rate)
+                q.save()
+
+            messages.success(request, "Rating has been submitted!")
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    out = list(Myrating.objects.filter(user=request.user.id).values())
+
+    # To display ratings in the movie detail page
+    movie_rating = 0
+    rate_flag = False
+    for each in out:
+        if each['movie_id'] == movie_id:
+            movie_rating = each['rating']
+            rate_flag = True
+            break
+
+    context = {'movies': movies,'movie_rating':movie_rating,'rate_flag':rate_flag,'update':update}
+    return render(request, 'detail.html', context)
+
+
+
+# TV shows/ Shows functionality
+def tv_shows(request):
+
+    shows = Show.objects.all()
+    query = request.GET.get('q')
+
+    if query:
+        shows = Show.objects.filter(Q(title__icontains=query)).distinct()
+        return render(request, 'search.html', {'shows': shows})
+
+    return render(request, 'mtv.html', {'shows': shows})
+
+def movies(request):
+
+    shows = Show.objects.all()
+    query = request.GET.get('q')
+
+    if query:
+        shows = Show.objects.filter(Q(title__icontains=query)).distinct()
+        return render(request, 'search.html', {'shows': shows})
+
+    return render(request, 'mtv.html', {'shows': shows})
+
+# recently added functionality
+def recently_added(request):
+
+    shows = Show.objects.all()
+    query = request.GET.get('q')
+
+    if query:
+        shows = Show.objects.filter(Q(title__icontains=query)).distinct()
+        return render(request, 'search.html', {'shows': shows})
+
+    return render(request, 'recently_added.html', {'shows': shows})
+
+# MyList functionality
+def mylist(request):
+
+    if not request.user.is_authenticated:
+        return redirect("login")
+    if not request.user.is_active:
+        raise Http404
+
+    movies = Show.objects.filter(mylist__watch=True,mylist__user=request.user)
+    query = request.GET.get('q')
+
+    if query:
+        movies = Show.objects.filter(Q(title__icontains=query)).distinct()
+        return render(request, 'mylist.html', {'movies': movies})
+
+    return render(request, 'mylist.html', {'movies': movies})
+
+
+# Register user
+def signUp(request):
+    form = UserForm(request.POST or None)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        user_id = form.cleaned_data['user_id']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(user_id=user_id, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect("index")
+
+    context = {'form': form}
+
+    return render(request, 'signup.html', context)
+
+
+# Login User
+def Login(request):
+    if request.method == "POST":
+        user_id = request.POST['user_id']
+        password = request.POST['password']
+        user = authenticate(user_id=user_id, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect("index")
+            else:
+                return render(request, 'login.html', {'error_message': 'Your account disabled'})
+        else:
+            return render(request, 'login.html', {'error_message': 'Invalid Login'})
+
+    return render(request, 'login.html')
+
+
+# Logout user
+def Logout(request):
+    logout(request)
+    return redirect("login")
